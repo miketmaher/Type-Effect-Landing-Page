7b86c09b094700dd8ce8e649870a9ec5d23419d5
Mike Maher <MikeM@corp.exchequer.ie>
2018-10-04 00:23:09 +0100

    converted TypeWriter to ES6 class

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-const TypeWriter = function(textElement, words, waitDuration = 3000) {
-  this.textElement = textElement;
-  this.words = words;
-  this.currentText = '';
-  this.index = 0;
-  this.waitDuration = parseInt(waitDuration, 10);
-  this.type();
-  this.isDeleting = false;
-};
-
-TypeWriter.prototype.type = function() {
-  const currentWordIndex = this.index % this.words.length;
-  const word = this.words[currentWordIndex];
-
-  if (this.isDeleting) {
-    this.currentText = word.substring(0, this.currentText.length - 1);
-  } else {
-    this.currentText = word.substring(0, this.currentText.length + 1);
-  }
-
-  this.textElement.innerHTML = `<span class="cursor">${
-    this.currentText
-  }</span>`;
-
-  let typeSpeed = 300;
-
-  if (this.isDeleting) {
-    typeSpeed /= 2;
-  }
-
-  if (!this.isDeleting && this.currentText === word) {
-    typeSpeed = this.waitDuration;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.currentText === '') {
+class TypeWriter {
+  constructor(textElement, words, waitDuration = 3000) {
+    this.textElement = textElement;
+    this.words = words;
+    this.currentText = '';
+    this.index = 0;
+    this.waitDuration = parseInt(waitDuration, 10);
+    this.type();
     this.isDeleting = false;
-    this.index++;
-    typeSpeed = 500;
   }
 
-  setTimeout(() => this.type(), typeSpeed);
-};
+  type() {
+    const currentWordIndex = this.index % this.words.length;
+    const word = this.words[currentWordIndex];
+
+    if (this.isDeleting) {
+      this.currentText = word.substring(0, this.currentText.length - 1);
+    } else {
+      this.currentText = word.substring(0, this.currentText.length + 1);
+    }
+
+    this.textElement.innerHTML = `<span class="cursor">${
+      this.currentText
+    }</span>`;
+
+    let typeSpeed = 300;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    if (!this.isDeleting && this.currentText === word) {
+      typeSpeed = this.waitDuration;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.currentText === '') {
+      this.isDeleting = false;
+      this.index++;
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
 
 document.addEventListener('DOMContentLoaded', init);
 
